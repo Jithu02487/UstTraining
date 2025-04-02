af71e2ad1ba5a8ed1f23b6c37250e9f0e7fa055a
Jithu <jithuofficial02487@gmail.com>
2025-04-02 12:01:52 +0530

    third

--- jenkins/Hello1.java
+++ jenkins/Hello1.java
@@ -1,6 +1,6 @@
 public class Hello1
 {
 	public static void main(String []args){
-		System.out.println("Hello Jithu its updated...");
+		System.out.println("Hello Jithu its updated, and git alert is working...");
 	}
 }
